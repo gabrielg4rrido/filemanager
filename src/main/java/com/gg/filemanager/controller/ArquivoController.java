ae90a32b9cbdcf269d9cf645a07e27c2312a7363
Gabriel Garrido <gabrielgarridoag@gmail.com>
2024-09-26 02:07:36 -0300

    Removendo parêntesis

--- src/main/java/com/gg/filemanager/controller/ArquivoController.java
+++ src/main/java/com/gg/filemanager/controller/ArquivoController.java
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(("/api/arquivos"))
+@RequestMapping("/api/arquivos")
 public class ArquivoController {
 
     @Autowired
--- src/main/java/com/gg/filemanager/controller/DiretorioController.java
+++ src/main/java/com/gg/filemanager/controller/DiretorioController.java
@@ -13,7 +13,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(("/api/diretorios"))
+@RequestMapping("/api/diretorios")
 public class DiretorioController {
 
     @Autowired
